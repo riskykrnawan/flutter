2cc726bc081fa7bda3e54fe0efbdb502e694b9dc
riskykrnawan <riskykrnawan@gmail.com>
2022-05-15 22:49:19 +0800

    tugas1_riskykurniawan

---
+++ tugas_1_pemmo_2009106050_riskykurniawan/lib/screens/page_three.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class PageThree extends StatelessWidget {
+  const PageThree({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/woman.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: SizedBox(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height*.7,
+                    ),
+                    Container(
+                      width: 170,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)
+                          ),
+                          primary: Colors.blueAccent,
+                        ), onPressed: () { Navigator.pop(context); },
+                        child:  Container(
+                            height: 25,
+                            alignment: Alignment.center,
+                            child: const Text("Back"),
+                          ),
+                        )
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height*.2,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ),
+        ],
+      ),
+      ),
+    );
+  }
+}
